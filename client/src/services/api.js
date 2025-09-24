6dd202e369c694c7b67726d23ef2a83a78e7e11d
sabari-0507 <sabarisivakumar2006@gmail.com>
2025-09-24 10:03:10 +0530

    your

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 // services/api.js
 import axios from "axios";
 
-export const BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000";
+export const BASE = process.env.REACT_APP_API_BASE || "https://my-financial-application-with-voice-01oy.onrender.com";
 const API = `/api/transactions`;
 
 // Shared API client (headers managed by AuthContext)
